7b153b84722e2be0c64605da4940621e93dc1280
Vladimir Kopitsa <vladimirkopitsa@Vladimirs-MacBook-Pro.local>
2017-09-30 20:41:14 +0300

    Init

---
+++ BatteryCurrent/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  BS
+//
+//  Created by Vladimir Kopitsa on 9/30/17.
+//  Copyright © 2017 Vladimir Kopitsa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+}
+
